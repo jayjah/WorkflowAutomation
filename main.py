a842de87c6385f6dd5f62adfa2b66e928301f4fa
Markus <krebs@taxi.de>
2019-10-21 20:03:23 +0200

    removed unused parts from main file

--- main.py
+++ main.py
@@ -2,39 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-def create_google_acc(firstname, lastname, vc, device):
-
-    # wait here longer cause of request to google
-    vc.sleep(8)
-    vc.dump(window=-1)
-    vc.findViewWithTextOrRaise(u'''Konto erstellen''').touch()
-    vc = wait(vc)
-    vc.findViewWithTextOrRaise(u'''Für mich selbst''').touch()
-    vc = wait(vc)
-    vc.findViewByIdOrRaise("firstName").type(firstname)
-    vc = wait(vc)
-    device.press('KEYCODE_ENTER')
-    vc = wait(vc)
-    vc.findViewByIdOrRaise("lastName").type(lastname)
-    vc = wait(vc)
-    device.press('KEYCODE_ENTER')
-    vc = wait(vc)
-    if vc.findViewByIdOrRaise("nameError") is not None:
-        # error handling
-        print ("error handling should be happenend here")
-    # if something like: Bist du ein Roboter error handling
-    vc.findViewByIdOrRaise("day").type(birthday)
-    vc = wait(vc)
-
-    vc.findViewByIdOrRaise("month-label").type(birthmonth)
-    vc = wait(vc)
-    vc.findViewByIdOrRaise("year").type(birthyear)
-
-
-
-
-
-
 
 
 
